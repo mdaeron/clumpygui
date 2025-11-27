82408b52a26c34bbe1d215d8be44b594a63ddd62
mdaeron <mathieu@daeron.fr>
2025-11-27 21:47:09 +0100

    Added typer callback

--- src/clumpygui/__init__.py
+++ src/clumpygui/__init__.py
@@ -6,11 +6,20 @@ app = typer.Typer(
 	rich_markup_mode = 'rich',
 	)
 
+@app.callback(invoke_without_command = True)
+def callback(ctx: typer.Context):
+	"""
+	A collection of clumpy web apps
+	"""
+	if ctx.invoked_subcommand is None:
+		typer.echo(ctx.get_help())
+		raise typer.Exit()
+
 @app.command(name = "D47calib")
 def D47calib():
-	'''
+	"""
 	Use D47calib library to convert between carbonate Δ47 and temperature using various calibrations
-	'''
+	"""
 	print('Hi from D47calib')
 
 @app.command(name = "D95thermo")
