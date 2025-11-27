7a56b9ee80b2f79dba31e41cb3d146b2924872d6
mdaeron <mathieu@daeron.fr>
2025-11-27 17:44:58 +0100

    Add commands with uppercase letters and docstrings

--- src/clumpygui/__init__.py
+++ src/clumpygui/__init__.py
@@ -1,2 +1,22 @@
+import typer
+
+app = typer.Typer(
+	add_completion = True,
+	context_settings={'help_option_names': ['-h', '--help']},
+	rich_markup_mode = 'rich',
+	)
+
+@app.command(name = "D47calib")
+def D47calib():
+	'''
+	Use D47calib library to convert between carbonate Δ47 and temperature using various calibrations
+	'''
+	print('Hi from D47calib')
+
+@app.command(name = "D95thermo")
+def D95thermo():
+	print('Hi from D95thermo')
+
 def main() -> None:
-    print("Hello from clumpygui!")
+	app()
+	
