75d0967568106b317f0e2b4cc92dfd1f5c812c05
mdaeron <mathieu@daeron.fr>
2025-11-27 22:17:07 +0100

    Added big banner

--- src/clumpygui/__init__.py
+++ src/clumpygui/__init__.py
@@ -9,6 +9,19 @@ app = typer.Typer(
 @app.callback(invoke_without_command = True)
 def callback(ctx: typer.Context):
 	"""
+	\b
+	 .d88b.  88                                    .d88b.  88     88 888888
+	d8P  Y8b 88                                   d8P  Y8b 88     88   88  
+	88    88 88                                   88    88 88     88   88  
+	88       88 88  88 8888b.db.  888b.  88  88   88       88     88   88  
+	88       88 88  88 88 "88 "8b 88 "8b 88  88   88  8888 88     88   88  
+	88    88 88 88  88 88  88  88 88  88 88  88   88    88 88     88   88  
+	Y8b  d8P 88 Y8b 88 88  88  88 88 d8P Y8b 88   Y8b  d8P Y8b. .d8P   88  
+	 "Y88P"  88  Y8888 88  88  88 888P"    "Y88    "Y888P8  "Y888P"  888888
+	                              88         88
+	                              88    Yb  d8P
+	                              88     "Y8P"
+	
 	A collection of clumpy web apps
 	"""
 	if ctx.invoked_subcommand is None:
